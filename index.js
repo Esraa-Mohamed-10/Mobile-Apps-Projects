ca40a614cc137a2b4ef797ed7ff386fc49822cfc
esraa-mohamed <esraamohamed1008@gmail.com>
2024-07-28 23:58:52 +0300

    about features section added

---
+++ index.js
@@ -0,0 +1,55 @@
+let fixed_btn = document.getElementById("fixed-btn")
+let menu_btn = document.getElementById("menu-btn")
+let close_menu = document.getElementById("close-menu")
+let drop_menu= document.getElementById("drop-menu")
+
+// console.log("scroll : " + scrollY)
+
+document.addEventListener("scroll",function()
+{
+    if(scrollY>=700)
+    {
+fixed_btn.style.display="block"
+    }
+    else if(scrollY<700)
+    {
+        fixed_btn.style.display="none"
+    }
+})
+
+menu_btn.addEventListener('click',function(){
+    menu_btn.style.display="none"
+    close_menu.style.display="block"
+    drop_menu.style.display="block"
+})
+
+close_menu.addEventListener('click',function(){
+    menu_btn.style.display="block"
+    close_menu.style.display="none"
+    drop_menu.style.display="none"
+})
+
+
+// progress bar animation
+document.addEventListener("DOMContentLoaded", () => {
+    const progressSection = document.querySelector(".progress-section");
+    const progressBars = document.querySelectorAll(".progress-bar");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            progressBars.forEach((bar) => {
+              bar.style.width = bar.getAttribute("data-progress") + "%";
+            });
+            observer.unobserve(progressSection); // Stop observing once the animation has started
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Trigger when 10% of the section is visible
+      }
+    );
+
+    observer.observe(progressSection);
+  });
